20e316727dbf005a6fba9e63d47dd43f2f6f7599
Florencia Díaz de la Fuente <dflorencia07@gmail.com>
2022-06-14 22:05:16 -0300

    cambios 1ra entrega

--- main.js
+++ main.js
@@ -1,86 +1,45 @@
-let entrada = prompt("buscás modelo femenino o masculino?").toLowerCase()
-let prototipo = Number (prompt ("Elegí el N° de prototipo o modelo"))
+let idSeleccionado = Number (prompt ("ingrese N° de ID del producto"))
 let cantidad = Number (prompt("Indique cantidad de camisetas"))
 let estampado = prompt("Querés estampar un N° y/o nombre en tus camisetas? SI/NO").toLowerCase()
-let precioEstampado = 300
-let precio = 0
+let precioEstampado = 0
 let iva = 1.21
+let total = 0
 
-function multiplicar1 (primerNum, segundoNum, tercerNum) {
-    return (primerNum * segundoNum * tercerNum)
+if (estampado === "si") {
+    precioEstampado = 300
 }
-function multiplicar2 (primerNum, segundoNum, tercerNum, cuartoNum) {
-    return ((primerNum + cuartoNum) * segundoNum * tercerNum)
+else {
+    precioEstampado = 0
+}
+
+if ((idSeleccionado > 12) || (cantidad == 0)) {
+    alert ("Los datos ingresados no son válidos")
 }
 
 
-if (entrada == "femenino") {
-    switch (prototipo) {
-        case 1:
-        case 2:
-        case 3:
-            precio = 3000
-            break
-        case 4: 
-        case 5: 
-        case 6:
-            precio = 3050
-            break
-        case 7: 
-        case 8: 
-        case 9:
-        case 10:
-            precio = 3100
-        default: alert ("El prototipo ingresado no es válido")
-            break
-    } if (estampado == "si") {
-        resultado = multiplicar2 (precio, cantidad, iva, precioEstampado)
-    alert("tu presupuesto es $" + resultado) 
-    }
-    else {resultado = multiplicar1 (precio, cantidad, iva)
-        alert("tu presupuesto es $" + resultado) }
-    
+const productos = [ {id: 1, modelo:"Classic", precio: 3000},
+                    {id: 3, modelo:"Liso", precio: 3050},
+                    {id: 5, modelo:"Argento", precio: 3100},
+                    {id: 7, modelo:"Banda", precio: 3120},
+                    {id: 9, modelo:"Floreado", precio: 3150},
+                    {id: 11, modelo:"Dúo", precio: 3200},
+                    {id: 2, modelo:"Classic", precio: 3200},
+                    {id: 4, modelo:"Liso", precio: 3230},
+                    {id: 6, modelo:"Argento", precio: 3250},
+                    {id: 8, modelo:"Banda", precio: 3280},
+                    {id: 10, modelo:"Sombra", precio: 3300},                   
+                    {id: 12, modelo:"Banda horizontal", precio: 3310}
+]
 
 
-} 
-else if (entrada == "masculino") {
-    switch (prototipo) {
-        case 1:
-        case 2:
-        case 3:
-            precio = 3200
-            break
-        case 4: 
-        case 5: 
-        case 6:
-            precio = 3250
-            break
-        case 7: 
-        case 8: 
-        case 9:
-        case 10:
-            precio = 3300
-        default: alert ("El prototipo ingresado no es válido")
-            break
-    }
-    if (estampado == "si") {
-        resultado = multiplicar2 (precio, cantidad, iva, precioEstampado)
-    alert("tu presupuesto es $" + resultado) 
-    }
-    else {resultado = multiplicar1 (precio, cantidad, iva)
-        alert("tu presupuesto es $" + resultado) }
-
-}
-else {alert("Los datos ingresados no son válidos")}
+const productoSeleccionado = productos.find((el) => el.id === idSeleccionado)
 
 
-const modelosFem = ["modelo1", "modelo2", "modelo3", "modelo4", "modelo5"]
-modelosFem.push ("modelo6", "modelo7", "modelo8", "modelo9", "modelo10")
+const carrito = []
+carrito.push(productoSeleccionado, cantidad, precioEstampado)
 
-console.log (modelosFem)
+total = (productoSeleccionado.precio * cantidad) + precioEstampado
 
-const modelosMasc1 = ["modelo1", "modelo2", "modelo3", "modelo4", "modelo5"]
-const modelosMasc2 = ["modelo6", "modelo7", "modelo8", "modelo9", "modelo10"]
-const totalModelosMasc = modelosMasc1.concat(modelosMasc2)
+console.log (carrito)
 
-console.log (totalModelosMasc)
+alert ("El total de su compra es: $" + total)
