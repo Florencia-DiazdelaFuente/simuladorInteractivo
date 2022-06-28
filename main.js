6b64ed2ae4c770e4dcb4f9cbc6bd1082461a466b
Florencia Díaz de la Fuente <dflorencia07@gmail.com>
2022-06-28 18:57:17 -0300

    cambios 2da entrega

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
-let iva = 1.21
-let total
+// Armado del stock
 
-
+const productos = []
 
 class Producto {
     constructor (id, modelo, precio, img){
@@ -11,11 +10,6 @@ class Producto {
     this.precio = precio;
     this.img = img;
 }}
-
-
-
-const productos = [
-]
 
 const producto1 = new Producto (1, "Classic",3000, "./assets/camiseta2.jpg");
 const producto3 = new Producto (3, "Liso",3050, "./assets/camiseta2.jpg");
@@ -28,16 +22,149 @@ const producto6 = new Producto (6, "Argento",3250, "./assets/camiseta1.jpg");
 const producto8 = new Producto (8, "Banda",3280, "./assets/camiseta1.jpg");
 const producto10 = new Producto (10, "Sombra",3300, "./assets/camiseta1.jpg");
 
-
 productos.push (producto1, producto3, producto5, producto7, producto9, producto2, producto4, producto6, producto8, producto10)
 
 
 
 
+// -- SELECTORES --
+
+const contenedorProductos = document.querySelector('.contenedorProductos')
+const contenedorCarrito = document.querySelector("#contenedorCarrito")
+let precioTotal = document.querySelector("#precioTotal")
+const btnVaciarCarrito = document.querySelector("#vaciarCarrito")
+const btnCarrito = document.querySelector(".btnCarrito")
+const modalContenedor = document.querySelector(".modalContenedor")
+let contadorCarrito = document.querySelector(".contadorCarrito")
+const btnCerrarCarrito = document.querySelector("#carritoCerrar")
+
+const carritoEnLS = JSON.parse( localStorage.getItem('carrito') )
 
 
-const carrito = []
 
+
+
+
+//--DOM de productos--
+
+
+productos.forEach((el) => {
+    const div = document.createElement ("div")
+    div.classList.add('fichaProducto')
+
+    div.innerHTML = ` 
+                <img class=imagenProducto src=${el.img} alt="camisetaF">
+                <p>ID: ${el.id}</p>
+                <p> Modelo: ${el.modelo}</p>
+                <p>$${el.precio}</p>
+                <button onclick="agregarAlCarrito(${el.id})" class="botonAgregar">Agregar <i> </i></button>
+                `
+
+contenedorProductos.append(div)
+}
+)
+
+
+
+
+//--CARRITO--
+
+let carrito = []
+
+function agregarAlCarrito (id) {
+    const item = productos.find((prod)=> prod.id === id)
+    carrito.push(item)
+    
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    renderCarrito()
+    renderTotal()
+    renderCantidad()
+}
+
+function eliminarDelCarrito (id) {
+    const item = productos.find((prod) =>prod.id === id)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    renderCarrito()
+    renderTotal()
+    renderCantidad()
+}
+
+function vaciarCarrito (id) {
+    carrito.length = 0
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    renderCarrito ()
+    renderTotal ()
+    renderCantidad()
+}
+
+btnVaciarCarrito.addEventListener("click", vaciarCarrito)
+
+
+
+const renderCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach((el) => {
+        const div = document.createElement("div")
+        div.classList.add("itemEnCarrito")
+
+        div.innerHTML = ` 
+                    <p>Modelo: ${el.modelo}</p>
+                    <p>$${el.precio} </p>
+                    <button onclick="eliminarDelCarrito(${el.id})"><i class="fa-solid fa-trash"></i></button>
+                    `
+        contenedorCarrito.append(div)
+    })
+}
+
+
+const renderCantidad = () => {
+    if (carrito.length >=1) {
+        contadorCarrito.innerHTML = carrito.length
+    }
+    else {
+        contadorCarrito.innerHTML = ""
+    }
+}
+
+const renderTotal = () => {
+    let total = 0
+    carrito.forEach((el) => {
+        total += el.precio
+    })
+    precioTotal.innerHTML = total
+}
+
+if (carritoEnLS) {
+    carrito = carritoEnLS
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+} else {
+    carrito = []
+}
+
+
+//--MODAL--
+
+btnCarrito.addEventListener ("click", ()=> {
+        modalContenedor.classList.toggle("modalActivo")
+})
+
+
+
+
+
+
+
+
+//--código viejo--
 
 const calculo = (a, b, c) => (a * b) + c 
 // total = calculo (productoSeleccionado.precio, cantidad, precioEstampado)
@@ -63,72 +190,5 @@ const calculo = (a, b, c) => (a * b) + c
 
 // const totalCarrito = document.getElementById ("carrito")
 // totalCarrito.innerText = "$" + totalConIva
-
-//...............EVENTOS
-
-const productosContainer = document.querySelector('.contenedor-productos')
-const contenedorCarrito = document.querySelector("#contenedorCarrito")
-let totalCarrito = document.querySelector("#totalCarrito")
-
-
-productos.forEach((el) => {
-    const div = document.createElement ("div")
-    div.classList.add()
-
-    div.innerHTML = ` 
-                <img  src=${el.img} alt="camisetaF">
-                <p> ${el.id}</p>
-                <p> ${el.modelo}</p>
-                <p>${el.precio}</p>
-                <button onclick="agregarAlCarrito(${el.id})" class="botonAgregar">Agregar <i> </i></button>
-                `
-
-productosContainer.append(div)
-}
-)
-
-//.......CARRITO
-
-function agregarAlCarrito (id) {
-    const item = productos.find((prod)=> prod.id === id)
-    carrito.push(item)
-    
-    console.log(carrito)
-    renderCarrito()
-    renderTotal()
-    
-}
-
-
-console.log (carrito)
-
-
-const renderCarrito = () => {
-    contenedorCarrito.innerHTML = ""
-    carrito.forEach((el) => {
-        const div = document.createElement("div")
-        div.classList.add()
-
-        div.innerHTML = ` 
-                    <p> ${el.modelo}</p>
-                    <p>${el.precio} </p>
-                    <button>Eliminar</button>
-                    `
-        contenedorCarrito.append(div)
-    })
-}
-
-const renderTotal = () => {
-    total = 0
-    carrito.forEach((el) => {
-        total += el.precio
-    })
-    totalCarrito.innerText = total
-
-    console.log(total)
-
-
-}
-
 
 
